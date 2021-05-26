212be98f89ca17bb5e499c83857bcfe008c5542a
ilan199929 <43996999+IlanKalendarov@users.noreply.github.com>
2021-05-26 17:41:48 +0300

    master

---
+++ RDPHook/ServerInterface.cs
@@ -0,0 +1,86 @@
+﻿// RemoteFileMonitor (File: FileMonitorHook\ServerInterface.cs)
+//
+// Copyright (c) 2017 Justin Stenning
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Please visit https://easyhook.github.io for more information
+// about the project, latest updates and other tutorials.
+
+using System;
+
+namespace RDPHook
+{
+    /// <summary>
+    /// Provides an interface for communicating from the client (target) to the server (injector)
+    /// </summary>
+    public class ServerInterface : MarshalByRefObject
+    {
+        public void IsInstalled(int clientPID)
+        {
+            //Console.WriteLine("FileMonitor has injected FileMonitorHook into process {0}.\r\n", clientPID);
+            //Console.WriteLine("Hook installed in mstsc.exe, PID {0}\r\n", clientPID);
+        }
+
+        /// <summary>
+        /// Output the message to the console.
+        /// </summary>
+        /// <param name="fileNames"></param>
+        public void ReportMessages(string[] messages)
+        {
+            for (int i = 0; i < messages.Length; i++)
+            {
+                Console.WriteLine(messages[i]);
+            }
+        }
+
+        public void ReportMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Report exception
+        /// </summary>
+        /// <param name="e"></param>
+        public void ReportException(Exception e)
+        {
+            Console.WriteLine("The target process has reported an error:\r\n" + e.ToString());
+        }
+
+        int count = 0;
+        /// <summary>
+        /// Called to confirm that the IPC channel is still open / host application has not closed
+        /// </summary>
+        public void Ping()
+        {
+            // Output token animation to visualise Ping
+            var oldTop = Console.CursorTop;
+            var oldLeft = Console.CursorLeft;
+            Console.CursorVisible = false;
+
+            var chars = "\\|/-";
+            Console.SetCursorPosition(Console.WindowWidth - 1, oldTop - 1);
+            Console.Write(chars[count++ % chars.Length]);
+
+            Console.SetCursorPosition(oldLeft, oldTop);
+            Console.CursorVisible = true;
+        }
+    }
+}
